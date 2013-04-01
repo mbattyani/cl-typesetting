4ee97a2e4008e73819d7c92855161bca0c280feb
mbattyani <marc@battyani.com>
2013-04-01 10:57:36 -0400

    fix clisp issue (probe-file can't handle directories on some platforms)

--- zzinit.lisp
+++ zzinit.lisp
@@ -3,7 +3,7 @@
 ;;; The homepage of cl-typesetting is here: http://www.fractalconcept.com/asp/html/cl-typesetting.html
 
 ;;
-;; Proposed initialize! function and warning when loading without hyphen-patterns directory, 
+;; Proposed initialize! function and warning when loading without hyphen-patterns directory,
 ;;
 ;; Dave Cooper, david -dot- cooper -at- genworks -dot- com
 ;;
@@ -15,10 +15,10 @@
 (defun initialize! (&key hyphen-patterns-directory afm-files-directories)
   "Directory. Set the hyphen-patterns directory to the correct runtime
 value, and force initialization of American and French hyphenation tries."
-  
-  (when afm-files-directories 
+
+  (when afm-files-directories
     (pdf:initialize! :afm-files-directories afm-files-directories))
-  
+
   (when (pdf::confirm-afm-files-directories)
     (setq *default-font* (pdf:get-font)
 	  *font* *default-font*))
@@ -32,7 +32,8 @@ value, and force initialization of American and French hyphenation tries."
 
 
 (defun confirm-hyphen-patterns-directory ()
-  (or (probe-file cl-typesetting-hyphen::*hyphen-patterns-directory*)
+  (or (#-clisp probe-file #+clisp ext:probe-directory
+	       cl-typesetting-hyphen::*hyphen-patterns-directory*)
       (warn "You have set the following non-existent hyphen-patterns directory:
 
 ~a
@@ -45,6 +46,3 @@ initialize the system with something like this:
 	    cl-typesetting-hyphen::*hyphen-patterns-directory*)))
 
 (initialize!)
-
-
-
